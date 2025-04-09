0e21c410cbaf752670af5ee51d781641cb858dee
proflaulier <cyrille.laulier@ac-rennes.fr>
2025-04-09 15:12:42 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,11 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(input.compassHeading())
 })
 input.calibrateCompass()
-serial.redirectToUSB()
+serial.redirect(
+SerialPin.P0,
+SerialPin.P1,
+BaudRate.BaudRate115200
+)
 basic.forever(function () {
     serial.writeNumber(Math.round(input.compassHeading()))
     basic.pause(200)
