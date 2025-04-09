ad997b9908be715a05761fa05133308127213253
proflaulier <cyrille.laulier@ac-rennes.fr>
2025-04-09 16:12:51 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,53 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(input.compassHeading())
+    cible = input.compassHeading()
+    basic.showNumber(cible)
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(cap)
+    bluetooth.uartWriteNumber(0)
+})
+let cap = 0
+let cible = 0
+bluetooth.startUartService()
 input.calibrateCompass()
-serial.redirect(
-SerialPin.P0,
-SerialPin.P1,
-BaudRate.BaudRate115200
-)
+cible = 0
 basic.forever(function () {
-    serial.writeNumber(Math.round(input.compassHeading()))
-    basic.pause(200)
+    cap = input.compassHeading()
+    if (Math.abs(cible - cap) < 5) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else {
+        if (cap < cible) {
+            pins.digitalWritePin(DigitalPin.P0, 0)
+            pins.digitalWritePin(DigitalPin.P1, 1)
+            pins.digitalWritePin(DigitalPin.P2, 0)
+            basic.showLeds(`
+                . . # . .
+                . . . # .
+                # # # # #
+                . . . # .
+                . . # . .
+                `)
+        } else {
+            pins.digitalWritePin(DigitalPin.P0, 0)
+            pins.digitalWritePin(DigitalPin.P1, 0)
+            pins.digitalWritePin(DigitalPin.P2, 0)
+            basic.showLeds(`
+                . . # . .
+                . # . . .
+                # # # # #
+                . # . . .
+                . . # . .
+                `)
+        }
+    }
+    basic.pause(100)
 })
