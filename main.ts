e35c5ecace5bf6b6ced755304c454aa6249fdd8e
proflaulier <cyrille.laulier@ac-rennes.fr>
2025-04-09 17:26:17 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,53 +1,67 @@
 input.onButtonPressed(Button.A, function () {
     cible = input.compassHeading()
     basic.showNumber(cible)
+    arret = 0
 })
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(cap)
-    bluetooth.uartWriteNumber(0)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    arret = 1
 })
 let cap = 0
 let cible = 0
-bluetooth.startUartService()
+let arret = 0
+arret = 1
+pins.digitalWritePin(DigitalPin.P0, 0)
+pins.digitalWritePin(DigitalPin.P1, 0)
+pins.digitalWritePin(DigitalPin.P2, 0)
 input.calibrateCompass()
 cible = 0
 basic.forever(function () {
     cap = input.compassHeading()
-    if (Math.abs(cible - cap) < 5) {
-        pins.digitalWritePin(DigitalPin.P0, 1)
-        pins.digitalWritePin(DigitalPin.P1, 0)
-        pins.digitalWritePin(DigitalPin.P2, 0)
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
-            . . # . .
-            `)
-    } else {
-        if (cap < cible) {
-            pins.digitalWritePin(DigitalPin.P0, 0)
-            pins.digitalWritePin(DigitalPin.P1, 1)
-            pins.digitalWritePin(DigitalPin.P2, 0)
-            basic.showLeds(`
-                . . # . .
-                . . . # .
-                # # # # #
-                . . . # .
-                . . # . .
-                `)
-        } else {
-            pins.digitalWritePin(DigitalPin.P0, 0)
+    if (arret == 0) {
+        if (Math.abs(cible - cap) < 5) {
+            pins.digitalWritePin(DigitalPin.P0, 1)
             pins.digitalWritePin(DigitalPin.P1, 0)
             pins.digitalWritePin(DigitalPin.P2, 0)
             basic.showLeds(`
                 . . # . .
-                . # . . .
-                # # # # #
-                . # . . .
+                . # # # .
+                # . # . #
+                . . # . .
                 . . # . .
                 `)
+        } else {
+            if (cap < cible) {
+                pins.digitalWritePin(DigitalPin.P0, 0)
+                pins.digitalWritePin(DigitalPin.P1, 1)
+                pins.digitalWritePin(DigitalPin.P2, 0)
+                basic.showLeds(`
+                    . . # . .
+                    . . . # .
+                    # # # # #
+                    . . . # .
+                    . . # . .
+                    `)
+            } else {
+                pins.digitalWritePin(DigitalPin.P0, 0)
+                pins.digitalWritePin(DigitalPin.P1, 0)
+                pins.digitalWritePin(DigitalPin.P2, 1)
+                basic.showLeds(`
+                    . . # . .
+                    . # . . .
+                    # # # # #
+                    . # . . .
+                    . . # . .
+                    `)
+            }
         }
+        basic.pause(100)
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
     }
-    basic.pause(100)
 })
